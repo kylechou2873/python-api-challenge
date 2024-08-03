ea412637b54f7fd0923f1a51e21cf3ab525051a5
kylechou2873 <43939307+kylechou2873@users.noreply.github.com>
2024-08-02 21:55:08 -0500

    Update api_keys.py

--- Starter_Code/api_keys.py
+++ Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "bcbc66249782233d45d27a539d5a0ef3"
+weather_api_key = "Key"
 
 # Geoapify API Key
-geoapify_key = "09670fb8e9c24ff090355802dcc0a7f9"
+geoapify_key = "Key"
